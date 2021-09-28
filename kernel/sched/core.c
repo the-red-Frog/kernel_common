bb66ddc6d3a0197325130ef81af070839f074acb
Elliot Berman <quic_eberman@quicinc.com>
2021-09-28 08:48:55 -0700

    NOUPSTREAM: ANDROID: kernel: Core Kernel Exports
    [CPNOTE: 30/09/21] Lee: GKI vendor exports
    
    Squash:
      NOUPSTREAM: ANDROID: GKI: time: Export for nsec_to_clock_t
      NOUPSTREAM: ANDROID: tick/nohz: export tick_nohz_get_sleep_length()
      NOUPSTREAM: ANDROID: GKI: pid: Export for find_task_by_vpid
      NOUPSTREAM: ANDROID: Export perf_event_read_local function
      NOUPSTREAM: ANDROID: PM: sleep: export device_pm_callback_start/end trace event
      NOUPSTREAM: ANDROID: perf: Export clock_set_rate tracepoint
      NOUPSTREAM: Revert "sched: Remove sched_setscheduler*() EXPORTs"
      NOUPSTREAM: ANDROID: Export some scheduler APIs for vendor modules
      NOUPSTREAM: ANDROID: GKI: core: Export for runqueues
      NOUPSTREAM: ANDROID: GKI: cputime: Export for thread_group_cputime_adjusted
      NOUPSTREAM: ANDROID: sched: export wake_up_if_idle()
      NOUPSTREAM: ANDROID: softirq: Export irq_handler_entry tracepoint
      NOUPSTREAM: ANDROID: sched: Export few scheduler symbols for vendor modules
      NOUPSTREAM: ANDROID: Sched: Export sched_feat_keys symbol needed by vendor modules
      NOUPSTREAM: ANDROID: sched/core: Export symbols needed by modules
      NOUPSTREAM: ANDROID: sched: Export symbols for vendor EAS wakup path function
      NOUPSTREAM: ANDROID: Sched: Export scheduler symbols needed by vendor modules
      NOUPSTREAM: ANDROID: cgroup: Export functions used by modules
      NOUPSTREAM: ANDROID: sched: Export key used by module
      NOUPSTREAM: ANDROID: sched: Export sched_switch tracepoint symbol
      NOUPSTREAM: ANDROID: sched: time: Export symbols needed for schedutil module
      NOUPSTREAM: ANDROID: dma: Rework so dev_get_cma_area() can be called from a module
      NOUPSTREAM: ANDROID: irqdesc: Export symbol for vendor modules
      ANDROID: sched: Export set_next_entity
      ANDROID: sched/fair: export sysctl_sched_latency symbol
      ANDROID: sched: Add export symbol resched_curr
      ANDROID: sched: Add export symbols for sched features
      ANDROID: sched: Export max_load_balance_interval
      ANDROID: printk: Export symbol for scheduler vendor hook
      ANDROID: sched: Export rq lock symbols
      ANDROID: sched: Export symbol for scheduler vendor module
      ANDROID: sched: Add parameter to android_rvh_set_iowait
      ANDROID: sched: export task_rq_lock
    
    Bug: 143964928
    Bug: 158067689
    Bug: 169136276
    Bug: 158067689
    Bug: 151972133
    Bug: 158528747
    Bug: 167449286
    Bug: 166776693
    Bug: 163613349
    Bug: 155241766
    Bug: 158067689
    Bug: 158067689
    Bug: 169136276
    Bug: 175806230
    Bug: 173468448
    Bug: 173559623
    Bug: 174219212
    Bug: 170507310
    Bug: 173725277
    Bug: 192873984
    Bug: 175045928
    Bug: 175448875
    Bug: 175806230
    Bug: 170511085
    Bug: 155218010
    Bug: 177393442
    Bug: 199823932
    Bug: 183674818
    Bug: 175448877
    Bug: 176077958
    Bug: 177050087
    Bug: 180125905
    Bug: 203530090
    Bug: 203580415
    Bug: 171598214
    Bug: 178340230
    Change-Id: I9667d55296a0c73115a7a55d27e406b1291104ef
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    Signed-off-by: Lee Jones <joneslee@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -111,8 +111,10 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(sched_overutilized_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_util_est_cfs_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_util_est_se_tp);
 EXPORT_TRACEPOINT_SYMBOL_GPL(sched_update_nr_running_tp);
+EXPORT_TRACEPOINT_SYMBOL_GPL(sched_switch);
 
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
+EXPORT_SYMBOL_GPL(runqueues);
 
 #ifdef CONFIG_SCHED_DEBUG
 /*
@@ -127,6 +129,7 @@ DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
 const_debug unsigned int sysctl_sched_features =
 #include "features.h"
 	0;
+EXPORT_SYMBOL_GPL(sysctl_sched_features);
 #undef SCHED_FEAT
 
 /*
@@ -560,6 +563,7 @@ void raw_spin_rq_lock_nested(struct rq *rq, int subclass)
 		raw_spin_unlock(lock);
 	}
 }
+EXPORT_SYMBOL_GPL(raw_spin_rq_lock_nested);
 
 bool raw_spin_rq_trylock(struct rq *rq)
 {
@@ -589,6 +593,7 @@ void raw_spin_rq_unlock(struct rq *rq)
 {
 	raw_spin_unlock(rq_lockp(rq));
 }
+EXPORT_SYMBOL_GPL(raw_spin_rq_unlock);
 
 #ifdef CONFIG_SMP
 /*
@@ -607,6 +612,7 @@ void double_rq_lock(struct rq *rq1, struct rq *rq2)
 
 	double_rq_clock_clear_update(rq1, rq2);
 }
+EXPORT_SYMBOL_GPL(double_rq_lock);
 #endif
 
 /*
@@ -632,6 +638,7 @@ struct rq *__task_rq_lock(struct task_struct *p, struct rq_flags *rf)
 			cpu_relax();
 	}
 }
+EXPORT_SYMBOL_GPL(__task_rq_lock);
 
 /*
  * task_rq_lock - lock p->pi_lock and lock the rq @p resides on.
@@ -674,6 +681,7 @@ struct rq *task_rq_lock(struct task_struct *p, struct rq_flags *rf)
 			cpu_relax();
 	}
 }
+EXPORT_SYMBOL_GPL(task_rq_lock);
 
 /*
  * RQ-clock updating methods:
@@ -754,6 +762,7 @@ void update_rq_clock(struct rq *rq)
 	rq->clock += delta;
 	update_rq_clock_task(rq, delta);
 }
+EXPORT_SYMBOL_GPL(update_rq_clock);
 
 #ifdef CONFIG_SCHED_HRTICK
 /*
@@ -1050,6 +1059,7 @@ void resched_curr(struct rq *rq)
 	else
 		trace_sched_wake_idle_without_ipi(cpu);
 }
+EXPORT_SYMBOL_GPL(resched_curr);
 
 void resched_cpu(int cpu)
 {
@@ -1352,6 +1362,7 @@ static struct uclamp_se uclamp_default[UCLAMP_CNT];
  *   * An admin modifying the cgroup cpu.uclamp.{min, max}
  */
 DEFINE_STATIC_KEY_FALSE(sched_uclamp_used);
+EXPORT_SYMBOL_GPL(sched_uclamp_used);
 
 /* Integer rounded range for each bucket */
 #define UCLAMP_BUCKET_DELTA DIV_ROUND_CLOSEST(SCHED_CAPACITY_SCALE, UCLAMP_BUCKETS)
@@ -1524,6 +1535,7 @@ unsigned long uclamp_eff_value(struct task_struct *p, enum uclamp_id clamp_id)
 
 	return (unsigned long)uc_eff.value;
 }
+EXPORT_SYMBOL_GPL(uclamp_eff_value);
 
 /*
  * When a task is enqueued on a rq, the clamp bucket currently defined by the
@@ -2109,6 +2121,7 @@ void activate_task(struct rq *rq, struct task_struct *p, int flags)
 
 	p->on_rq = TASK_ON_RQ_QUEUED;
 }
+EXPORT_SYMBOL_GPL(activate_task);
 
 void deactivate_task(struct rq *rq, struct task_struct *p, int flags)
 {
@@ -2116,6 +2129,7 @@ void deactivate_task(struct rq *rq, struct task_struct *p, int flags)
 
 	dequeue_task(rq, p, flags);
 }
+EXPORT_SYMBOL_GPL(deactivate_task);
 
 static inline int __normal_prio(int policy, int rt_prio, int nice)
 {
@@ -2208,6 +2222,7 @@ void check_preempt_curr(struct rq *rq, struct task_struct *p, int flags)
 	if (task_on_rq_queued(rq->curr) && test_tsk_need_resched(rq->curr))
 		rq_clock_skip_update(rq);
 }
+EXPORT_SYMBOL_GPL(check_preempt_curr);
 
 #ifdef CONFIG_SMP
 
@@ -3164,8 +3179,8 @@ void set_task_cpu(struct task_struct *p, unsigned int new_cpu)
 
 	__set_task_cpu(p, new_cpu);
 }
+EXPORT_SYMBOL_GPL(set_task_cpu);
 
-#ifdef CONFIG_NUMA_BALANCING
 static void __migrate_swap_task(struct task_struct *p, int cpu)
 {
 	if (task_on_rq_queued(p)) {
@@ -3280,7 +3295,7 @@ int migrate_swap(struct task_struct *cur, struct task_struct *p,
 out:
 	return ret;
 }
-#endif /* CONFIG_NUMA_BALANCING */
+EXPORT_SYMBOL_GPL(migrate_swap);
 
 /*
  * wait_task_inactive - wait for a thread to unschedule.
@@ -3831,6 +3846,7 @@ void wake_up_if_idle(int cpu)
 out:
 	rcu_read_unlock();
 }
+EXPORT_SYMBOL_GPL(wake_up_if_idle);
 
 bool cpus_share_cache(int this_cpu, int that_cpu)
 {
@@ -4892,6 +4908,7 @@ struct callback_head balance_push_callback = {
 	.next = NULL,
 	.func = (void (*)(struct callback_head *))balance_push,
 };
+EXPORT_SYMBOL_GPL(balance_push_callback);
 
 static inline struct callback_head *
 __splice_balance_callbacks(struct rq *rq, bool split)
@@ -7203,6 +7220,7 @@ int available_idle_cpu(int cpu)
 
 	return 1;
 }
+EXPORT_SYMBOL_GPL(available_idle_cpu);
 
 /**
  * idle_task - return the idle task for a given CPU.
@@ -7700,11 +7718,13 @@ int sched_setscheduler(struct task_struct *p, int policy,
 {
 	return _sched_setscheduler(p, policy, param, true);
 }
+EXPORT_SYMBOL_GPL(sched_setscheduler);
 
 int sched_setattr(struct task_struct *p, const struct sched_attr *attr)
 {
 	return __sched_setscheduler(p, attr, true, true);
 }
+EXPORT_SYMBOL_GPL(sched_setattr);
 
 int sched_setattr_nocheck(struct task_struct *p, const struct sched_attr *attr)
 {
@@ -7730,6 +7750,7 @@ int sched_setscheduler_nocheck(struct task_struct *p, int policy,
 {
 	return _sched_setscheduler(p, policy, param, false);
 }
+EXPORT_SYMBOL_GPL(sched_setscheduler_nocheck);
 
 /*
  * SCHED_FIFO is a broken scheduler model; that is, it is fundamentally
@@ -9631,7 +9652,9 @@ int in_sched_functions(unsigned long addr)
  * Every task in system belongs to this group at bootup.
  */
 struct task_group root_task_group;
+EXPORT_SYMBOL_GPL(root_task_group);
 LIST_HEAD(task_groups);
+EXPORT_SYMBOL_GPL(task_groups);
 
 /* Cacheline aligned slab cache for task_group */
 static struct kmem_cache *task_group_cache __read_mostly;
